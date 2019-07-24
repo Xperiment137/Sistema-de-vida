f494a81141e96db996b36415f248665ec1d1b12d
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2019-07-24 15:59:16 +0200

    Add files via upload

---
+++ Sistema de Vida/Daño.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daño : MonoBehaviour {
+    public int daño = 0;
+    public GameObject aux;
+    private Vidamanager manager;
+
+
+    void Awake()
+    {
+       aux=GameObject.FindGameObjectWithTag("Player");
+        manager = aux.GetComponent<Vidamanager>();
+    }
+   
+
+	void Update () {
+        manager.Vida -= daño;
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            daño = 3;
+        }
+    }
+        void OnCollisionExit(Collision col)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                daño = 0;
+            }
+
+
+
+        }
+
+
+    }
---
+++ Sistema de Vida/Vidamanager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Vidamanager : MonoBehaviour
+{
+
+    public int Vida = 100;
+    public int Municion = 0;
+    public Text texto;
+    public Font fuente;
+    public int daños;
+  
+  
+
+   void Update()
+    {
+       
+        if (Vida > 0)
+        {
+            texto.text = Vida.ToString();
+            texto.color = Color.black;
+            texto.font = fuente;
+        }
+        else
+        {
+
+            texto.text = "Estas muerto";
+            Color newColor = new Color(156, 64, 7);
+            texto.color = newColor;
+            texto.font = fuente;
+        }
+    }
+        
+   
+  
+}
+    
